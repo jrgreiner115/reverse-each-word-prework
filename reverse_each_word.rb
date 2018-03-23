7aa998595498eedd1611337b355f6b6bc0e40e2a
Joe Greiner <jrgreiner115@gmail.com>
2018-03-23 17:42:33 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,10 +5,3 @@ def reverse_each_word(string)
    sentence << x.reverse}
    sentence.join(" ")
 end
-
-def reverse_collect_word(string)
-  new_array = string.split
-  new_array.collect{|x|
-   new_array << x.reverse}
-   new_array.join(" ")
-end
